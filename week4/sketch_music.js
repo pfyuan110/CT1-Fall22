5ae889a0ca421551dd97628a35e1fc5a61a66d98
pfyuan110 <pfyuan110@gmail.com>
2022-10-11 22:19:08 -0700

    Update sketch_music.js

--- week4/sketch_music.js
+++ week4/sketch_music.js
@@ -5,13 +5,13 @@ let button
 function setup() {
     createCanvas(200, 200)
     song = loadSound('Paolo Pavan - Heart.mp3')
-    button = createButton('play')
-    button.mousePressed(togglePlaying)
     background(0)
 }
 
 function loaded() {
     console.log('loaded')
+    button = createButton('play')
+    button.mousePressed(togglePlaying)
 }
 
 function togglePlaying() {
@@ -22,9 +22,5 @@ function togglePlaying() {
         song.pause()
         button.html('play')
     }
-
-}
-
-function draw() {
 
 }
